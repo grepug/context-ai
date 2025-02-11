9a9314d9af7a52f5b26a94e3322eb3ba6c0f88df
Kai Shao <grepug@icloud.com>
2025-02-11 11:58:57 +0800

    Update .gitignore to include .vscode and refactor TextCompletionKey to simplify client task creation

--- Sources/ContextAI/AICompletions/TextCompletionKey.swift
+++ Sources/ContextAI/AICompletions/TextCompletionKey.swift
@@ -10,22 +10,12 @@ public enum TextCompletionKey: String, Hashable, Sendable {
 }
 
 extension TextCompletionKey {
-    var template: String {
-        switch self {
-        case .collocations: PromptTemplates.collocations
-        case .translator: PromptTemplates.translator
-        case .studyNotes: PromptTemplates.studyNotes
-        case .thesaurus: PromptTemplates.thesaurus
-        case .memorizingHelper: PromptTemplates.memorizingHelper
-        case .usages: PromptTemplates.usages
-        }
-    }
-
-    public func makeCompletion(input: AICompletionNormalizedInput) -> AIStaticTextCompletion<AICompletionNormalizedInput> {
+    public func makeTaskForClient(input: AICompletionNormalizedInput) -> AIStaticTextCompletion<AICompletionNormalizedInput> {
         .init(
             key: rawValue,
             input: input,
-            staticTemplate: template
+            // for client, we don't need to provide the static template
+            staticTemplate: ""
         )
     }
 }
